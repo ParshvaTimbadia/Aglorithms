a7ea47911b9f7b28a0fe544bb0cc775aeddb5634
Parshva Timbadia <parshwat@gmail.com>
2020-05-09 23:13:05 +0530

    Create Start of LinkedList Cycle.py

---
+++ Medium/Start of LinkedList Cycle.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+
+
+class Node:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+  def print_list(self):
+    temp = self
+    while temp is not None:
+      print(temp.value, end='')
+      temp = temp.next
+    print()
+
+
+
+  def find_cycle_start(head):
+
+    slow, fast = head, head
+    while fast is not None and fast.next is not None:
+      fast = fast.next.next
+      slow = slow.next
+      if slow == fast:  # found the cycle
+        slow=head
+        break
+
+    while slow!= fast:
+      fast=fast.next
+      slow=slow.next
+
+    return slow
+
+ 
+
+
+def main():
+  head = Node(1)
+  head.next = Node(2)
+  head.next.next = Node(3)
+  head.next.next.next = Node(4)
+  head.next.next.next.next = Node(5)
+  head.next.next.next.next.next = Node(6)
+
+  head.next.next.next.next.next.next = head.next.next
+  print("LinkedList cycle start: " + str(find_cycle_start(head).value))
+
+  head.next.next.next.next.next.next = head.next.next.next
+  print("LinkedList cycle start: " + str(find_cycle_start(head).value))
+
+  head.next.next.next.next.next.next = head
+  print("LinkedList cycle start: " + str(find_cycle_start(head).value))
+
+
+main()
